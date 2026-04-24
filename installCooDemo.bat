3c34b24e8f16e455955ee4f3b865f1aba75ef3c2
Chihiro Ito <chiroito107@gmail.com>
2026-04-24 11:59:04 +0900

    OpenShift版とCOO版のインストーラを作成

---
+++ installCooDemo.bat
@@ -0,0 +1,8 @@
+oc apply -f openshift-coo\project.yaml
+oc apply -f openshift-coo\jfr.yaml
+oc apply -f openshift-coo\log.yaml
+oc apply -f openshift-coo\metrics.yaml
+oc apply -f openshift-coo\trace.yaml
+oc apply -f openshift-coo\monitoring.yaml
+oc apply -f openshift-coo\application.yaml
+oc start-build -n observability-coo-demo hello-app-build-config
---
+++ prepDemo.bat
@@ -0,0 +1,2 @@
+oc apply -f openshift-prep\install-observability-operators.yaml
+oc apply -f openshift-prep\minio.yaml
