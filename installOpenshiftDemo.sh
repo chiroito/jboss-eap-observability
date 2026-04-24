03323fb22236ff9a4d8a29b1259d0fb2990dbf0d
Chihiro Ito <chiroito107@gmail.com>
2026-04-24 15:09:55 +0900

    インストールシェルを追加

---
+++ installOpenshiftDemo.sh
@@ -0,0 +1,8 @@
+oc apply -f ./openshift/project.yaml
+oc apply -f ./openshift/jfr.yaml
+oc apply -f ./openshift/log.yaml
+oc apply -f ./openshift/metrics.yaml
+oc apply -f ./openshift/trace.yaml
+oc apply -f ./openshift/monitoring.yaml
+oc apply -f ./openshift/application.yaml
+oc start-build -n observability-demo hello-app-build-config
